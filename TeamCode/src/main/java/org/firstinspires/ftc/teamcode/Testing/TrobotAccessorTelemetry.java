cb541886e9d754db471b8715462103fe55a23878
Tikki <ticui21@icstudents.org>
2020-07-03 12:54:38 -0500

    Testing accessor and mutator telemtry

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/TrobotAccessorTelemetry.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode.Testing;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.SourceFiles.Trobot;
+
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a four wheeled robot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@TeleOp(name = "Tank Mode", group = "TeleOp")
+public class TrobotAccessorTelemetry extends LinearOpMode {
+    public Trobot trobot;
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        trobot = new Trobot(hardwareMap);
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        trobot.getRuntime().reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            telemetry.addData("Claw", trobot.getComponent().getClaw());
+            telemetry.addData("Elevator", trobot.getComponent().getElevator());
+            telemetry.update();
+        }
+    }
+}
